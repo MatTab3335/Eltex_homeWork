0d378bce0d47af1052f63fbe6e65eaa0233aa869
MatTab3335 <75005423+MatTab3335@users.noreply.github.com>
2022-04-30 20:26:25 +0700

    Add files via upload
    Решение задания по замене определенного байта в числе

---
+++ replace_byte.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int origin = 0xFAFAFAFA; //11111010 11111010 11111010 11111010
+int byte;
+
+
+int main(void)
+{
+
+  printf("Enter a byte in hex: ");
+  scanf("%x", &byte);
+  printf("You entered: %x\n", byte);
+  origin &= 0xFF00FFFF;               //make 3d byte empty
+  byte <<= 16;
+  origin |= byte;                     //fill 3d byte
+  printf("New number is: %x\n", origin);    //nice!
+  return 0;
+}
