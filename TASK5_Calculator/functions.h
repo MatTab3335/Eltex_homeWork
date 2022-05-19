af13c21309d703f8a70340ea1568bdea5a3d16c2
MatTab3335 <75005423+MatTab3335@users.noreply.github.com>
2022-05-19 12:35:05 +0700

    Create functions.h

---
+++ TASK5_Calculator/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+
+#endif /* FUNCTIONS_H_ */
