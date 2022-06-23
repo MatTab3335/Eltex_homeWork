c33edf712f955c9062fdfea68f79a031106b4400
MatTab3335 <75005423+MatTab3335@users.noreply.github.com>
2022-06-23 23:18:05 +0700

    Update manager.c

--- TASK8_FILE_MANAGER/manager.c
+++ TASK8_FILE_MANAGER/manager.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
-#include <string.h>
-#include "syscalls.h"
-#include <fcntl.h>      /* флажки чтения и записи */
 #include <sys/types.h> /* определения типов */
+#include <dirent.h>
 #include <sys/stat.h>   /* структура, возвращаемая stat */
-#include "dirent.h"
+#include <string.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <fcntl.h>      /* флажки чтения и записи */
 #include <malloc.h>
 
 #define MAX_PATH 1024
+#define COMMAND_SIZE 10
 
 void print_names(char *);
 void dirwalk(char *);
-int stat(char *, struct stat *);
-void resize_list(char **, int);
-void goToFile(void);
+char **resize_list(char **, int);
+void empty_stdin();
 
 static int nfiles = 0;
 static char **name_list;
 
-int main (int argc, char **argv)
+int main (int argc, char const *argv)
 {
-    char *command;           //user command
+    char command[COMMAND_SIZE];           //user command
     int pointer = 0;
     //-------Allocate memory---------
-    name_list = (char**)alloc(nfiles * sizeof(char*));
+    name_list = (char**)malloc(nfiles * sizeof(char*));
     for (int i = 0; i < nfiles; i++) 
-        name_list[i] = (char*)alloc(MAX_PATH*sizeof(char));
+        name_list[i] = (char*)malloc(MAX_PATH*sizeof(char));
     //-------Go walk through directories--------
     dirwalk(".");
     while(1) {
         //ПОЛУЧИТЬ КОМАНДУ
-        if (strcmp(command, "quit") == 0)
+        printf("Enter the № of file or 'quit' to exit\n");
+        fgets(command, COMMAND_SIZE, stdin);
+
+        if (strcmp(command, "quit\n") == 0)
             return 0;
         else
             if ((pointer = atoi(command)))
@@ -43,16 +47,16 @@ int main (int argc, char **argv)
     return 0;
 }
 
-void goToFile(int p)
-{
-    printf("Enter a number (1 to max number) to go to file\n");
-    //ПОЛУЧИТЬ НОМЕР
-    dirwalk(name_list[p - 1]);
+void empty_stdin() {
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
 }
 void print_names(char *name)
 {
     struct stat stbuf;
-    if (stat(name, stbuf) == -1) {
+    if (stat(name, &stbuf) == -1) {
         fprintf(stderr, "print_names: нет доступа к %s\n", name);
         return;
     }
@@ -63,23 +67,23 @@ void dirwalk(char *dir)
     char name[MAX_PATH];
     nfiles = 0;
 
-    Dirent *dp;
+    struct dirent *dp;
     DIR *dfd;
 
     if ((dfd == opendir(dir)) == NULL) {
-        fprintf(stderr, "dirwalk: не могу открыть %s\n", );
+        fprintf(stderr, "dirwalk: не могу открыть %s\n", dir);
         return;
     }
     while ((dp == readdir(dfd)) != NULL) {
         /*if (strcmp(dp->name, ".") == 0 
             || strcmp(dp->name, "..") == 0)
             continue;  */                         //пропустить себя и родителя
-        if (strlen(dir) + strlen(dp->name) + 2 > sizeof(name))
+        if (strlen(dir) + strlen(dp->d_name) + 2 > sizeof(name))
             fprintf(stderr, "dirwalk: слишком длинное имя %s/%s\n", 
-                dir, dp->name);
+                dir, dp->d_name);
         else {
             nfiles++;
-            sprintf(name, "%s/%s". dir, dp->name);
+            sprintf(name, "%s/%s", dir, dp->d_name);
             print_names(name);
             name_list = resize_list(name_list, nfiles);
             strcpy(name_list[nfiles - 1], name);
