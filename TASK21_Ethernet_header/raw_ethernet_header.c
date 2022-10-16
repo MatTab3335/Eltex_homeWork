4faa352bfab632296b93fe2e029c498fc428633f
MatTab3335 <75005423+MatTab3335@users.noreply.github.com>
2022-10-16 15:53:14 +0700

    1

---
+++ TASK21_Ethernet_header/raw_ethernet_header.c
@@ -0,0 +1,121 @@
+#include <stdio.h>	
+#include <string.h> 
+#include <sys/socket.h>	
+#include <stdlib.h> 
+#include <errno.h> 
+#include <linux/if_packet.h>
+#include <netinet/udp.h>	//Provides declarations for tcp header
+#include <netinet/ip.h>		//Provides declarations for ip header
+#include <netinet/ether.h>	//Provides declarations for ethernet header
+#include <net/if.h>
+#include <arpa/inet.h> 		// inet_addr
+#include <unistd.h> 		// sleep()
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define SERVER_PORT 5000
+#define MY_IP "192.168.0.105"
+#define MY_MAC "00:d8:61:88:7c:4a"
+#define SERV_IP "192.168.0.103"
+#define SERV_MAC "b8:27:eb:d6:37:21"
+
+int csum(struct iphdr *buf);
+
+int main (void)
+{
+	//Create a raw socket
+	int s = socket (AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if(s == -1)
+		handle_error("socket");
+	
+	//Datagram to represent the packet
+	char datagram[4096], *data;
+	
+	//zero out the packet buffer
+	memset (datagram, 0, 4096);
+	
+	//ethernet header
+	struct ether_header *ethh = (struct ether_header *)(datagram);
+	memset (ethh, 0, sizeof(struct ether_header));
+	struct ether_addr *eth_addr;
+	//IP header
+	struct iphdr *iph = (struct iphdr *)(datagram + sizeof(struct ether_header));
+	memset (iph, 0, sizeof(struct iphdr));
+	//UDP header
+	struct udphdr *udph = (struct udphdr *)(datagram + sizeof(struct ether_header) 
+							+ sizeof(struct iphdr));
+	memset (udph, 0, sizeof(struct udphdr));
+	struct sockaddr_ll addr_ll;
+
+	
+	//Data part
+	data = datagram + sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct udphdr);
+	strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	
+	//some address resolution
+	addr_ll.sll_family = AF_PACKET;
+	addr_ll.sll_ifindex = htons(if_nametoindex("enp3s0"));
+	addr_ll.sll_halen = htons(6);
+	eth_addr = ether_aton(SERV_MAC);
+	memcpy(addr_ll.sll_addr, eth_addr, sizeof(eth_addr));
+	
+	//Ethernet header
+	eth_addr = ether_aton(MY_MAC);
+	memcpy(ethh->ether_dhost, eth_addr, sizeof(eth_addr));
+	eth_addr = ether_aton(SERV_MAC);
+	memcpy(ethh->ether_shost, eth_addr, sizeof(eth_addr));
+	ethh->ether_type = htons(ETH_P_IP);
+
+	//Fill in the IP Header
+	iph->ihl = 5;
+	iph->version = 4;
+	iph->tos = 0;
+	iph->tot_len = sizeof(struct iphdr) + sizeof(struct udphdr); 
+	iph->id = htons(12345);
+	iph->frag_off = 0;
+	iph->ttl = 255;
+	iph->protocol = IPPROTO_UDP;
+	iph->check = 0;		
+	iph->saddr = inet_addr(MY_IP);		
+	iph->daddr = inet_addr(SERV_IP);
+	
+	
+	//UDP Header
+	udph->source = htons(5005);
+	udph->dest = htons(SERVER_PORT);
+	udph->len = htons(sizeof(struct udphdr) + strlen(data));
+	udph->check = 0;		//no need in IPv4
+	
+	// calculate the checksum for integrity
+  	iph->check = csum(iph);
+
+	while (1)
+	{
+		//Send the packet
+		if (sendto(s, datagram, strlen(datagram),	0, (struct sockaddr *) &addr_ll, sizeof (addr_ll)) < 0)
+			perror("sendto failed");
+		else
+			printf ("Packet Send \n");
+        // sleep for 1 seconds
+        sleep(1);
+	}
+	
+	return 0;
+}
+
+int csum(struct iphdr *buf)
+{
+	int sum = 0;
+	short *ptr = (short *)buf;
+
+	for (int i = 0; i < 10; i++) {
+		sum = sum + ptr;
+		ptr++;
+	}
+	int tmp = sum >> 16;
+	sum = (sum & 0xFFFF) + tmp;
+	sum = ~sum;
+
+	return sum;
+}
---
+++ TASK21_Ethernet_header/udp_server.c
@@ -0,0 +1,62 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 5000
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+/*struct sockaddr_in {
+    sa_family_t    sin_family; // address family: AF_INET 
+    in_port_t      sin_port;   // port in network byte order 
+    struct in_addr sin_addr;   // internet address 
+};
+
+// Internet address. 
+struct in_addr {
+    uint32_t       s_addr;     // address in network byte order 
+};*/
+
+
+int main(int argc, char *argv[])
+{
+    int udp_s_s, udp_s_c;
+    struct sockaddr_in my_addr, client_addr;
+    int client_addr_size = 0;
+    char in_buf[4096] = {};
+    char *ptr;
+
+    udp_s_s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_s_s == -1)
+        handle_error("socket");
+
+    memset(&my_addr, 0, sizeof(struct sockaddr_in));
+                        
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    my_addr.sin_port = htons(SERVER_PORT);
+
+
+    if (bind(udp_s_s, (struct sockaddr *) &my_addr,
+            sizeof(struct sockaddr_in)) == -1)
+        handle_error("bind");
+
+
+
+    client_addr_size = sizeof(struct sockaddr_in);
+                
+    while(1) {
+        recvfrom(udp_s_s, in_buf, sizeof(in_buf), 0, 
+                (struct sockaddr *) &client_addr, &client_addr_size);
+        ptr = in_buf;
+        printf("[MSG]: %s\n", ptr);
+    }
+
+}
